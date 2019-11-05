24c994ab9760979e1e809637f5442a53e253497c
Petr Evstifeev <villain.88@mail.ru>
2019-11-05 22:23:00 +0300

    writer work fine, refactoring to class

--- main.cpp
+++ main.cpp
@@ -18,14 +18,6 @@
 
 using namespace std;
 
-size_t file_part = -1;
-ifstream in;
-ofstream out;
-size_t block_size = 1024*1024;
-size_t blocks_count = 0;
-size_t last_section_size = block_size;
-const int digets_size = 16;
-
 class Signature
 {
 public:
@@ -34,11 +26,12 @@ public:
         in_data_queue_.setMaxSize(thread_count*10);
         out_data_queue_.setMaxSize(thread_count*10);
         isStart_ = true;
+        isWriterStart_ = true;
         threads_.reserve(thread_count);
         for(auto i = 0; i < thread_count; i++) {
             threads_.push_back(thread([=] {workerLoop(i+1);}));
         }
-        threads_.push_back(thread([=] {writeData();}));
+        writerThread_ = thread([=] {writeData();});
     }
 
     void force_stop()
@@ -52,12 +45,17 @@ public:
     {
         isStart_ = false;
         cv_data_available_.notify_all();
-        cv_write_available_.notify_all();
+
         for(auto &th: threads_) {
             if(th.joinable()) {
                 th.join();
             }
         }
+
+        isWriterStart_ = false;
+        cv_write_available_.notify_all();
+        if(writerThread_.joinable())
+            writerThread_.join();
     }
 
     void appendData(unique_ptr<block_data> data)
@@ -68,18 +66,21 @@ public:
 
 private:
     atomic_bool isStart_ = {false};
+    atomic_bool isWriterStart_ = {false};
     vector<thread> threads_;
+    thread writerThread_;
     condition_variable cv_data_available_;
     mutex cv_data_available_mutex_;
     condition_variable cv_write_available_;
     mutex cv_write_available_mutex_;
     DataQueue in_data_queue_;
     DataQueue out_data_queue_;
+    const int digets_size_ = 16;
     void workerLoop(int thread_id)
     {
         while(1) {
             if(isStart_) {
-                std::unique_lock<std::mutex> lock(cv_data_available_mutex_);
+                unique_lock<mutex> lock(cv_data_available_mutex_);
                 cv_data_available_.wait(lock);
             } else {
                 break;
@@ -93,13 +94,12 @@ private:
 
     void calcHash(unique_ptr<block_data> data)
     {
-        uint8_t result[digets_size];
+        uint8_t result[digets_size_];
         md5(reinterpret_cast<uint8_t*>(data->get_data()), data->get_data_size(), result);
         stringstream stringStream;
-        for (int i = 0; i < digets_size; i++)
-            stringStream << setfill('0') << setw(2) << std::hex  << static_cast<unsigned int>(result[i]);
+        for (int i = 0; i < digets_size_; i++)
+            stringStream << setfill('0') << setw(2) << hex  << static_cast<unsigned int>(result[i]);
         string md5sum = stringStream.str();
-        //cout << "Block " << data->get_block_num() << " md5 " << md5sum <<endl;
         unique_ptr<block_data> write_data(new block_data(md5sum.length(), data->get_block_num()));
         memcpy(write_data->get_data(), md5sum.c_str(), write_data->get_data_size());
         out_data_queue_.push(move(write_data));
@@ -113,8 +113,8 @@ private:
         out.open(outname, ios::out | ios::trunc);
 
         while(1) {
-            if(isStart_) {
-                std::unique_lock<std::mutex> lock(cv_write_available_mutex_);
+            if(isWriterStart_) {
+                unique_lock<mutex> lock(cv_write_available_mutex_);
                 cv_write_available_.wait(lock);
             } else {
                 break;
@@ -129,58 +129,82 @@ private:
     }
 };
 
-bool readBlock(char* buffer, size_t block_num)
+class Reader
 {
-    in.seekg(block_num*block_size, ios::beg);
-    if(block_num+1 != blocks_count) {
-       in.read(buffer, block_size);
-    } else {
-        memset(buffer, 0, block_size);
-        in.read(buffer, last_section_size);
+public:
+    Reader(const string &filename, size_t block_size):filename_(filename), block_size_(block_size)
+    {
+
     }
 
-    if (!in)
-        throw std::runtime_error("Error reading file");
+    int startReader()
+    {
+        in.open(filename_, ios::in | ios::binary);
+        if (!in) {
+            cout << "Cannot open file.\n" << endl;
+            return 1;
+        }
+        struct stat st;
+        if(stat(filename_.c_str(), &st) < 0) {
+          cerr << "stat failed, fname = "
+                    << filename_ << ", " << strerror(errno) << endl;
+          return 1;
+        }
 
-    return true;
-}
+        last_section_size_ = block_size_;
 
+        size_t filesize = (size_t)st.st_size;
+        cout << "Filesize: " << filesize << endl;
+        blocks_count_ = filesize/block_size_;
+        size_t remaining_part = filesize % block_size_;
+        if(remaining_part) {
+            blocks_count_++;
+            last_section_size_ = remaining_part;
+        }
+        cout << "Blocks count: " << blocks_count_ << endl;
+
+        Signature signature;
+        for(size_t i = 0; i < blocks_count_; i++) {
+            unique_ptr<block_data> data(new block_data(block_size_, i));
+            readBlock(data->get_data(), data->get_block_num());
+            signature.appendData(move(data));
+        }
+        signature.join();
+        in.close();
+    }
+
+private:
+    const string filename_;
+    const size_t block_size_;
+    size_t blocks_count_;
+    size_t last_section_size_;
+    ifstream in;
+
+    bool readBlock(char* buffer, size_t block_num)
+    {
+        in.seekg(block_num*block_size_, ios::beg);
+        //check for last section
+        if(block_num+1 != blocks_count_) {
+           in.read(buffer, block_size_);
+        } else {
+            memset(buffer, 0, block_size_);
+            in.read(buffer, last_section_size_);
+        }
+
+        if (!in)
+            throw runtime_error("Error reading file");
+
+        return true;
+    }
+};
 
 int main()
 {
     const char* fname = "/home/petr/tmp_file";
-    const char* outname = "/home/petr/tmp_file_md5";
-    in.open(fname, ios::in | ios::binary);
-    if (!in) {
-        cout << "Cannot open file.\n";
-        return 1;
-    }
-    struct stat st;
-    if(stat(fname, &st) < 0) {
-      std::cerr << "stat failed, fname = "
-                << fname << ", " << strerror(errno) << std::endl;
-      return 1;
-    }
 
-    size_t filesize = (size_t)st.st_size;
-    cout << "filesize " << filesize << endl;
-    blocks_count = filesize/block_size;
-    size_t remaining_part = filesize % block_size;
-    if(remaining_part) {
-        blocks_count++;
-        last_section_size = remaining_part;
-    }
-    cout << "blocks count " << blocks_count << endl;
-    out.open(outname, ios::out | ios::trunc);
-
-    Signature signature;
-    for(size_t i = 0; i < blocks_count; i++) {
-        unique_ptr<block_data> data(new block_data(block_size, i));
-        readBlock(data->get_data(), data->get_block_num());
-        signature.appendData(move(data));
-    }
-    signature.join();
-    in.close();
+    size_t block_size = 1024*1024;
+    Reader reader(fname, block_size);
+    reader.startReader();
 
     return 0;
 }
